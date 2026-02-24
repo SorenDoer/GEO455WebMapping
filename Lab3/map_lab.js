96ba8e0b376a34853ff39d0f0f75543b4337a271
SorenDoer <soren.doerfler@gmail.com>
2026-02-24 12:31:19 -0600

    Add files via upload

--- Lab3/map_lab.js
+++ Lab3/map_lab.js
@@ -13,128 +13,128 @@ var myIcon1 = L.icon({
     popupAnchor: [0, -28],
 });
 var myIcon2 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_2.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon3 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_3.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon4 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_4.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon5 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_5.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon6 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_6.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon7 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_7.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon8 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_8.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon9 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_9.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon10 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_10.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon11 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_11.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 var myIcon12 = L.icon({
-    iconUrl: 'images/icon_1.png',
+    iconUrl: 'images/icon_12.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -28],
 });
 
-var GNP = L.marker([48.77196847768131, -113.79183384940235])
+var GNP = L.marker([48.77196847768131, -113.79183384940235], {icon: myIcon1})
   .addTo(map)
   .bindPopup("<b>Glacier National Park")
   .openPopup();
 
-var vashon = L.marker([47.4227453, -122.4756722])
+var vashon = L.marker([47.4227453, -122.4756722], {icon: myIcon2})
   .addTo(map)
   .bindPopup("<b>Vashon Island")
   .openPopup();
 
-var rainier = L.marker([46.85282244, -121.766365146106])
+var rainier = L.marker([46.85282244, -121.766365146106], {icon: myIcon3})
   .addTo(map)
   .bindPopup("<b>Mount Rainier")
   .openPopup();
 
-var portland = L.marker([45.51646544, -122.6697931])
+var portland = L.marker([45.51646544, -122.6697931], {icon: myIcon4})
   .addTo(map)
   .bindPopup("<b>Portland")
   .openPopup();
 
-var eureka = L.marker([40.80109572, -124.1480268])
+var eureka = L.marker([40.80109572, -124.1480268], {icon: myIcon5})
   .addTo(map)
   .bindPopup("<b>Eureka")
   .openPopup();
 
-var burney = L.marker([41.01225773, -121.6520213])
+var burney = L.marker([41.01225773, -121.6520213], {icon: myIcon6})
   .addTo(map)
   .bindPopup("<b>Burney Falls")
   .openPopup();
 
-var lassen = L.marker([40.4907433, -121.5056231])
+var lassen = L.marker([40.4907433, -121.5056231], {icon: myIcon7})
   .addTo(map)
   .bindPopup("<b>Lassen Peak")
   .openPopup();
 
-var reno = L.marker([39.53623364, -119.7800493])
+var reno = L.marker([39.53623364, -119.7800493], {icon: myIcon8})
   .addTo(map)
   .bindPopup("<b>Reno")
   .openPopup();
 
-var tahoe = L.marker([39.08368912, -120.0419405])
+var tahoe = L.marker([39.08368912, -120.0419405], {icon: myIcon9})
   .addTo(map)
   .bindPopup("<b>Lake Tahoe")
   .openPopup();
 
-var bishop = L.marker([37.36812247, -118.4405931])
+var bishop = L.marker([37.36812247, -118.4405931], {icon: myIcon10})
   .addTo(map)
   .bindPopup("<b>Bishop")
   .openPopup();
 
-var moab = L.marker([38.56806025, -109.5165922])
+var moab = L.marker([38.56806025, -109.5165922], {icon: myIcon11})
   .addTo(map)
   .bindPopup("<b>Moab")
   .openPopup();
 
-var chihushus = L.marker([39.63989249, -105.8465971])
+var chihushus = L.marker([39.63989249, -105.8465971], {icon: myIcon12})
   .addTo(map)
   .bindPopup("<b>Chihuahua Lake")
   .openPopup();
