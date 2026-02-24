cca6a85c3f55aaa2c620535180d361dd04e71287
SorenDoer <soren.doerfler@gmail.com>
2026-02-23 19:02:21 -0600

    Add files via upload

---
+++ Lab3/map_lab.js
@@ -0,0 +1,7 @@
+const map = L.map("map").setView([43.812809378833286, -91.21050607445362], 13);
+
+L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+  maxZoom: 17,
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+               '<a href="https://opentopomap.org">OpenTopoMap</a>'
+}).addTo(map);
